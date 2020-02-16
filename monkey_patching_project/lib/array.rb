fa157d8a131a1067d57f7191aca0212781133621
Gary Miller <gary.miller@gmx.com>
2020-02-16 15:01:57 -0500

    Update array.rb
    Adds in the average method solution.

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,9 +1,18 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
+  
   def span
     if self.length > 0
     self.max - self.min
     else nil
     end
   end
+  
+  def average
+    if self.length > 0
+        self.inject(:+) / self.length.to_f
+    else nil
+    end
+  end
+  
 end
