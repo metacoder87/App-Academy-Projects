9213c88d8e40314863194aa5c4aa92f96115ac41
Gary Miller <gary.miller@gmx.com>
2020-02-16 15:01:23 -0500

    Update array.rb
    Adding the span method solution.

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,9 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-
+  def span
+    if self.length > 0
+    self.max - self.min
+    else nil
+    end
+  end
 end
