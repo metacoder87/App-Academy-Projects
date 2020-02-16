13989a93ab8383084688bac836ad201a1d69bad3
Gary Miller <gary.miller@gmx.com>
2020-02-15 20:33:01 -0500

    Update bootcamp.rb
    Partial solution to the Bootcamp class assignment.

--- Gary_Miller_solutions_bootcamp_project/lib/bootcamp.rb
+++ Gary_Miller_solutions_bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,35 @@
 class Bootcamp
-  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new() { |hash, k| hash[k] = [] } 
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def student_capacity
+    @student_capacity
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def grades
+    @grades
+  end
+
 end
