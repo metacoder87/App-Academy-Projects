e9181e63818a259ab5563ed642bf1020a348e8fa
Gary Miller <gary.miller@gmx.com>
2020-02-19 14:04:58 -0500

    Update guessing_game.rb
    Adds the initialize method for the GuessingGame class.

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,9 @@
 class GuessingGame
-
+    def initialize(min, max)
+        @min = min
+        @max = max
+        @secret_num = rand(@min..@max)
+        @num_attempts = 0
+        @game_over = false
+    end
 end
