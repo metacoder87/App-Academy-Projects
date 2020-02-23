ac06b24b117a6e6819c170e1107efe7ea786c00b
Gary Miller <gary.miller@gmx.com>
2020-02-23 04:11:22 -0500

    Update hotel.rb
    Adds in the initialize method solution.

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -2,4 +2,11 @@ require_relative "room"
 
 class Hotel
   
+  def initialize(name, vacancies)
+    @name = name
+    @rooms = {}
+
+    vacancies.each { |key, val| @rooms[key] = Room.new(val) }
+  end
+  
 end
