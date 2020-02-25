94c06ef2171956691e38434837c656a060bebc23
Gary Miller <gary.miller@gmx.com>
2020-02-25 14:37:46 -0500

    Update startup.rb
    Adds in the initialize method solution as well as the getters.

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,14 @@
 require "employee"
 
 class Startup
+  
+  attr_accessor :name, :funding, :salaries, :employees
 
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+  
 end
