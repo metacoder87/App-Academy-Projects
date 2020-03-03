b76d4e121d94bd7149679b3e3ad1d2802b53f343
Gary Miller <gary.miller@gmx.com>
2020-03-03 15:45:31 -0500

    Update code.rb
    Sweetening the syntax before I start Part 2.

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -5,40 +5,41 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
-  
+
+  #Part1
+
+  attr_accessor :pegs
+
   def self.valid_pegs?(arr)
     arr.all? { |char| POSSIBLE_PEGS.include?(char.upcase) }
   end
-  
-  attr_reader :pegs
-  
+
+  def self.random(num)
+    pegs_arr = []
+    num.times { pegs_arr << POSSIBLE_PEGS.keys.sample }
+    
+    Code.new(pegs_arr)
+  end
+
+  def self.from_string(str)
+    Code.new(str.split(''))
+  end
+
   def initialize(pegs)
     if Code.valid_pegs?(pegs)
       @pegs = pegs.map { |peg| peg.upcase }
     else raise "Error"
     end
   end
-  
-  def self.random(num)
-    pegs_arr = []
-    count = num
-    while count > 0
-      pegs_arr.push(POSSIBLE_PEGS.keys.sample)
-      count -= 1
-    end
-    Code.new(pegs_arr)
-  end
-  
-  def self.from_string(str)
-    Code.new(str.split(''))
-  end
-  
+
   def [](i)
     @pegs[i]
   end
-  
+
   def length
     @pegs.length
   end
-
+  
+  #Part 2
+  
 end
