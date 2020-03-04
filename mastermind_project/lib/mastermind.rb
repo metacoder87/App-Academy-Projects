cb081cd892337f99ddc6dde8d27bc0daa18c61ca
Gary Miller <gary.miller@gmx.com>
2020-03-03 19:26:14 -0500

    Update mastermind.rb
    Adds in the print_matches method solution.

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -2,9 +2,13 @@ require_relative "code"
 
 class Mastermind
   
-  def initialize(num)
-        @length = num 
+    def initialize(num)
         @secret_code = Code.random(num)
+    end
+
+    def print_matches(code)
+        puts "exact matches #{ @secret_code.num_exact_matches(code) }"
+        puts "near matches #{ @secret_code.num_near_matches(code) }"
     end
   
 end
