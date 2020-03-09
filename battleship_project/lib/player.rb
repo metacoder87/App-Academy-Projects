fe46fbf083ab0348d4c3f73653cf0b389269f888
Gary Miller <gary.miller@gmx.com>
2020-03-08 21:44:53 -0400

    Update player.rb
    Adds in the get_move method solution.

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
+  
+    def get_move
+        print "enter a position with coordinates seperated with a space like '4 7'"
+        move = gets.chomp.split(" ")
+        move.map! { |ele| ele.to_i }
+    end
 
 end
