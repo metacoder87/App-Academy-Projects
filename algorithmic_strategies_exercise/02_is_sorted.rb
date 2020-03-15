8590c86c7033daf49c1c7f15df820eb9a55fb4e4
Gary Miller <gary.miller@gmx.com>
2020-03-14 23:47:24 -0400

    Update 02_is_sorted.rb
    Adds in the is_sorted method solution.

--- algorithmic_strategies_exercise/02_is_sorted.rb
+++ algorithmic_strategies_exercise/02_is_sorted.rb
@@ -3,6 +3,14 @@
 # Do not use the built-in Array#sort in your solution :)
 
 
+def is_sorted(arr)
+    (0...arr.length - 1).each do |i| 
+        return false if arr[i] > arr[i+1]
+    end
+    true
+end
+
+
 p is_sorted([1, 4, 10, 13, 15])       # => true
 p is_sorted([1, 4, 10, 10, 13, 15])   # => true
 p is_sorted([1, 2, 5, 3, 4 ])         # => false
