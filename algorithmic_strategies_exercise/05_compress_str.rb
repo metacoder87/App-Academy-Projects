6bc04cb3c1384c470ab06bbc58ad320cadfae330
Gary Miller <gary.miller@gmx.com>
2020-03-14 23:50:33 -0400

    Update 05_compress_str.rb
    Adds in the compress_str method solution.

--- algorithmic_strategies_exercise/05_compress_str.rb
+++ algorithmic_strategies_exercise/05_compress_str.rb
@@ -3,6 +3,25 @@
 # For example "aaabbc" is compressed to "3a2bc".
 
 
+def compress_str(str)
+    compd = []
+    arr = str.split('')
+    count = 1
+    arr.each_with_index do |char, i|
+        if char == arr[i+1]
+            count += 1
+        elsif char != arr[i+1] && count == 1
+            compd << char
+        elsif char != arr[i+1] && count > 1
+            compd << count
+            compd << char
+            count = 1
+        end
+    end
+    compd.join('')
+end
+
+
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
 p compress_str("qqqqq")         # => "5q"
