51ad96d16311f324335fadc9522de03d2df90892
Gary Miller <gary.miller@gmx.com>
2020-03-16 17:20:47 -0400

    Update part_2.rb
    Adds in the hipsterfy method solution.

--- rspec_exercise_1/lib/part_2.rb
+++ rspec_exercise_1/lib/part_2.rb
@@ -1,2 +1,13 @@
 #RSPEC Exercise_1
 #Part_2
+
+def hipsterfy(str)
+    arr = str.split('')
+    vowel = 'aeiou'
+    if arr.none? { |char| vowel.include?(char) }
+        return str
+    end
+    i = str.chars.rindex { |char| vowel.include?(char) }
+    arr.delete_at(i)
+    arr.join('')
+end
