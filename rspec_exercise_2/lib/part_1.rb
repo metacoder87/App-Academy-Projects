560c02fd482776efd22285fd9ea988719909585c
Gary Miller <gary.miller@gmx.com>
2020-03-17 13:00:57 -0400

    Update part_1.rb
    Adds in the partition method solution.

--- rspec_exercise_2/lib/part_1.rb
+++ rspec_exercise_2/lib/part_1.rb
@@ -1,2 +1,10 @@
 #Part_1
 #RSPEC_Excercise_2
+
+def partition(arr, num)
+    lil = arr.select { |ele| ele < num }
+    big = arr.select { |ele| ele >= num }
+    parti = []
+    parti << lil 
+    parti << big
+end
