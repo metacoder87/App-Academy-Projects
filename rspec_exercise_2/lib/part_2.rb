754306f7e389d23c28b2e05029f70f9b2d38cde0
Gary Miller <gary.miller@gmx.com>
2020-03-17 14:32:43 -0400

    Update part_2.rb
    Adds in the palindrome? method solution.

--- rspec_exercise_2/lib/part_2.rb
+++ rspec_exercise_2/lib/part_2.rb
@@ -1,2 +1,11 @@
 #Part_2
 #RSPEC_Excercise_2
+
+def palindrome?(str)
+    rev = []
+    str.split("").reverse_each { |ele| rev << ele }
+    if str == rev.join("")
+        true
+    else false
+    end
+end
