6c152daf2c0201004f649279294add639d34bdf8
Gary Miller <gary.miller@gmx.com>
2020-03-21 12:16:59 -0400

    Update fuzz_buzz.rb
    Adds in my fixes for the fuzz_buzz method.

--- debugging_exercise_1/lib/fuzz_buzz.rb
+++ debugging_exercise_1/lib/fuzz_buzz.rb
@@ -9,7 +9,9 @@ def fuzz_buzz(num)
     numbers = []
 
     (1..num).each do |i|
-        if i % 3 != 0 || i % 5 != 0
+        
+        # needs to && not ||
+        if i % 3 != 0 && i % 5 != 0
             numbers << i
         end
     end
