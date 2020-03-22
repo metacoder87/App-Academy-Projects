0f589008530a51d48a4666bae1cd2febfb1888b8
Gary Miller <gary.miller@gmx.com>
2020-03-22 02:44:53 -0400

    Update problems.rb
    Adds in the largest_prime_factor method solution using prime? helper method.

--- debugging_exercise_2/lib/problems.rb
+++ debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,12 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+def prime?(num)
+    (2...num).none? { |div| num % div == 0 }
+end
+
+def largest_prime_factor(num)
+    divs = (2..num).select { |div| num % div == 0 && prime?(div) }
+    divs.max
+end
