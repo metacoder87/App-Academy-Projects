77509d009d2871f5204d022a2bdffc52598a4d96
Gary Miller <gary.miller@gmx.com>
2020-03-22 07:04:23 -0400

    Update part_2.rb
    Adds in the any_passing_students? method solution.

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -8,3 +8,7 @@ end
 def no_valid_url?(arr)
     arr.none? { |url| url.end_with?('.com','.net','.io','.org') }
 end
+
+def any_passing_students?(arr)
+    arr.any? { |hash| (hash[:grades].inject(:+) / hash[:grades].length) > 74 }
+end
