a8b026fd4a5aae761b91b30147263afa012472fb
Gary Miller <gary.miller@gmx.com>
2020-03-22 07:03:36 -0400

    Update part_2.rb
    Adds in the no_valid_url? method solution.

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -4,3 +4,7 @@
 def all_words_capitalized?(arr)
     arr.all? { |word| word.capitalize == word }
 end
+
+def no_valid_url?(arr)
+    arr.none? { |url| url.end_with?('.com','.net','.io','.org') }
+end
