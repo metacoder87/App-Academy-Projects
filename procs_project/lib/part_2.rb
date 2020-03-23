995200d25a88f32be1aaa21e80a24ed7944e22c7
Gary Miller <gary.miller@gmx.com>
2020-03-22 20:39:29 -0400

    Update part_2.rb
    Adds in the word_changer method solution.

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -7,3 +7,7 @@ def reverser(str, &prc)
     arr = [str.split('').reverse.join('')]
     arr.map! { |string| prc.call(string) }[0]
 end
+
+def word_changer(str, &prc)
+    str.split(' ').map! { |word| prc.call(word) }.join(' ')
+end
