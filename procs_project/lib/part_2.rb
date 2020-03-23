23f5615aa4a6bf36be54cbd301475333d7ba1a1c
Gary Miller <gary.miller@gmx.com>
2020-03-22 20:40:17 -0400

    Update part_2.rb
    Adds in the greater_proc_value method solution.

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -11,3 +11,10 @@ end
 def word_changer(str, &prc)
     str.split(' ').map! { |word| prc.call(word) }.join(' ')
 end
+
+def greater_proc_value(num, prc_1, prc_2)
+    if prc_1.call(num) > prc_2.call(num)
+        prc_1.call(num) 
+    else prc_2.call(num)
+    end
+end
