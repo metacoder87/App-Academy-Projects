9b16a51a6135c0dbe233a05fb44e8829c1de5cdc
Gary Miller <gary.miller@gmx.com>
2020-03-22 20:40:54 -0400

    Update part_2.rb
    Adds in the and_selector method solution.

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -18,3 +18,7 @@ def greater_proc_value(num, prc_1, prc_2)
     else prc_2.call(num)
     end
 end
+
+def and_selector(arr, prc_1, prc_2)
+    arr.select { |ele| prc_1.call(ele) && prc_2.call(ele) }
+end
