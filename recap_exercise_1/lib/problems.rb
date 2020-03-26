c0d22fde6c540d2b298e5b6418f44f2333a34384
Gary Miller <gary@gmcubedesigns.com>
2020-03-26 16:28:36 -0400

    Add files via upload

---
+++ recap_exercise_1/lib/problems.rb
@@ -0,0 +1,134 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/App-Academy-Projects
+
+
+require 'byebug'
+
+# Write a method, all_vowel_pairs, that takes in an array of words and returns all pairs of words
+# that contain every vowel. Vowels are the letters a, e, i, o, u. A pair should have its two words
+# in the same order as the original array. 
+#
+# Example:
+#
+# all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
+def all_vowel_pairs(words)
+    word_pair = []
+    vowel_pairs = []
+    vowel_hsh = Hash.new()
+    words.map { |word| vowel_hsh[word] = vowels(word) }
+    
+    perms = vowel_hsh.values.combination(2).to_a
+    v_perms = perms.select { |pair| all_vowels?(pair) }
+    
+    v_perms.map do |pair| 
+        pair.each do |ele|
+            word_pair << vowel_hsh.key(ele)
+        end
+        vowel_pairs << word_pair.join(" ")
+        word_pair.clear
+    end
+    vowel_pairs
+end
+
+def all_vowels?(arr)
+    arr.inject(:+).split('').sort.uniq.join('') == 'aeiou'
+end
+
+def vowels(word)
+    word.split('').select { |char| 'aeiou'.include?(char) }.join('')
+end
+
+# Write a method, composite?, that takes in a number and returns a boolean indicating if the number
+# has factors besides 1 and itself
+#
+# Example:
+#
+# composite?(9)     # => true
+# composite?(13)    # => false
+def composite?(num)
+    (2...num).any? { |fac| num % fac == 0 }
+end
+
+
+# A bigram is a string containing two letters.
+# Write a method, find_bigrams, that takes in a string and an array of bigrams.
+# The method should return an array containing all bigrams found in the string.
+# The found bigrams should be returned in the the order they appear in the original array.
+#
+# Examples:
+#
+# find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+# find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
+def find_bigrams(str, bigrams)
+    bi_ray = []
+    bigrams.each { |ele| str.split(" ").each { |word| word.include?(ele) ? bi_ray << ele : ele } }
+    bi_ray
+end
+
+class Hash
+    # Write a method, Hash#my_select, that takes in an optional proc argument
+    # The method should return a new hash containing the key-value pairs that return
+    # true when passed into the proc.
+    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
+    #
+    # Examples:
+    #
+    # hash_1 = {x: 7, y: 1, z: 8}
+    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+    #
+    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+    # hash_2.my_select                            # => {4=>4}
+    def my_select(&prc)
+        self.filter { |k, v| prc != nil ? prc.call(k, v) : k == v }
+    end
+end
+
+class String
+    # Write a method, String#substrings, that takes in a optional length argument
+    # The method should return an array of the substrings that have the given length.
+    # If no length is given, return all substrings.
+    #
+    # Examples:
+    #
+    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
+    # "cats".substrings(2)  # => ["ca", "at", "ts"]
+    def substrings(length = nil)
+        subs = []
+        (0...self.length).each do |start_i|
+            (start_i...self.length).each do |end_i|
+                subs << self[start_i..end_i]
+            end
+        end
+        subs.select do |ele| if length
+            ele.length == length 
+        else ele
+        end
+      end
+    end
+
+
+    # Write a method, String#caesar_cipher, that takes in an a number.
+    # The method should return a new string where each char of the original string is shifted
+    # the given number of times in the alphabet.
+    #
+    # Examples:
+    #
+    # "apple".caesar_cipher(1)    #=> "bqqmf"
+    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+    # "zebra".caesar_cipher(4)    #=> "difve"
+    def caesar_cipher(num)
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        sifed = self.chars.map do |char|
+            i = alphabet.index(char) 
+            new_alpha = alphabet.rotate(num)
+            char = new_alpha[i]
+        end
+        sifed.join('')
+    end
+end
+
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/App-Academy-Projects
