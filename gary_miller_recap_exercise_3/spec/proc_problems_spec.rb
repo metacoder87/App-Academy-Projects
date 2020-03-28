c139112bd184eec00b0781720c974b0460ce3a2e
Gary Miller <gary.miller@gmx.com>
2020-03-28 03:10:52 -0400

    Update proc_problems_spec.rb
    Adds in the spec tests for String#select method.

--- gary_miller_recap_exercise_3/spec/proc_problems_spec.rb
+++ gary_miller_recap_exercise_3/spec/proc_problems_spec.rb
@@ -1,1 +1,15 @@
 require "proc_problems"
+
+describe "String" do
+        describe "#select" do
+        it "should return a new string containing characters of the original string that return true when passed into the block" do
+            expect("app academy".select { |ch| !"aeiou".include?(ch) }).to eq("pp cdmy")
+            expect("HELLOworld".select { |ch| ch == ch.upcase }).to eq("HELLO")
+        end
+
+        it "should return the empty string if no block is passed" do
+            expect("HELLOworld".select).to eq("")
+            expect("app academy".select).to eq("")
+        end
+    end
+end
