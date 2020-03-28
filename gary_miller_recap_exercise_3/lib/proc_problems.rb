0a0e104db784d245bd6ae4c54872fa9e53fd83e8
Gary Miller <gary.miller@gmx.com>
2020-03-28 02:31:49 -0400

    Upate proc_problems.rb
    Adds in the Proc problems assignment details from App Academy.

--- gary_miller_recap_exercise_3/lib/proc_problems.rb
+++ gary_miller_recap_exercise_3/lib/proc_problems.rb
@@ -1,3 +1,27 @@
 
 
     # Proc Problems
+
+class String
+
+ # Extend the string class by defining a String#select method that 
+ # accepts a block. The method should return a new string containing 
+ # characters of the original string that return true when passed into 
+ # the block. If no block is passed, then return the empty string. 
+ # Do not use the built-in Array#select in your solution.
+
+    def select(&prc)
+
+    end
+
+ # Extend the string class by defining a String#map! method that accepts 
+ # a block. The method should modify the existing string by replacing 
+ # every character with the result of calling the block, passing in the 
+ # original character and it's index. Do not use the built-in Array#map 
+ # or Array#map! in your solution.
+
+    def map(&prc)
+
+    end
+
+end
