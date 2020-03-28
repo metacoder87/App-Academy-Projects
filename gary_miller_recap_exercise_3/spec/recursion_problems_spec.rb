e416de46536d61d1cda237fcda861deb3741836c
Gary Miller <gary.miller@gmx.com>
2020-03-28 03:06:49 -0400

    Update recursion_problems_spec.rb
    Adds in the spec test cases for multiply recursive method.

--- gary_miller_recap_exercise_3/spec/recursion_problems_spec.rb
+++ gary_miller_recap_exercise_3/spec/recursion_problems_spec.rb
@@ -1,1 +1,22 @@
 require "recursion_problems"
+
+describe "multiply" do
+    it "should takes in two numbers and returns their product" do
+        expect(multiply(3, 5)).to eq(15)
+        expect(multiply(5, 3)).to eq(15)
+        expect(multiply(2, 4)).to eq(8)
+        expect(multiply(0, 10)).to eq(0)
+    end
+
+    it "should not use the multiplication(*) operator" do
+        expect(multiply).to_not receive(:*)
+        expect(multiply(-3, -6)).to eq(18)
+        expect(multiply(3, -6)).to eq(-18)
+        expect(multiply(-3, 6)).to eq(-18)
+    end
+
+    it "should be recursive" do
+        expect(self).to receive(:multiply).at_least(:twice).and_call_original
+        multiply(-3, 6)
+    end
+end
