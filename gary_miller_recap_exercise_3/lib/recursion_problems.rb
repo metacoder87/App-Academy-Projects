cdf5fea69251e17d307162eba320c25248e51757
Gary Miller <gary.miller@gmx.com>
2020-03-28 02:33:18 -0400

    Update recursion_problems.rm
    Adds in the Recursion assignment details from App Academy.

--- gary_miller_recap_exercise_3/lib/recursion_problems.rb
+++ gary_miller_recap_exercise_3/lib/recursion_problems.rb
@@ -1,3 +1,43 @@
 
     
     # Recursion Problems
+
+ # Write a method multiply(a, b) that takes in two numbers 
+ # and returns their product.
+
+ # You must solve this recursively (no loops!)
+ # You must not use the multiplication (*) operator
+
+def multiply(a, b)
+
+end
+
+ # The Lucas Sequence is a sequence of numbers. The first number of 
+ # the sequence is 2. The second number of the Lucas Sequence is 1. 
+ # To generate the next number of the sequence, we add up the previous 
+ # two numbers. For example, the first six numbers of the sequence 
+ # are: 2, 1, 3, 4, 7, 11, ...
+
+ # Write a method lucasSequence that accepts a number representing 
+ # a length as an arg. The method should return an array containing 
+ # the Lucas Sequence up to the given length. Solve this recursively.
+
+def lucas_sequence(length)
+
+end
+
+ # The Fundamental Theorem of Arithmetic states that every positive 
+ # integer is either a prime or can be represented as a product of 
+ # prime numbers.
+
+ # Write a method prime_factorization(num) that accepts a number and 
+ # returns an array representing the prime factorization of the given 
+ # number. 
+ 
+ # This means that the array should contain only prime numbers 
+ # that multiply together to the given num. The array returned should 
+ # contain numbers in ascending order. Do this recursively.
+
+def prime_factorization(num)
+
+end
