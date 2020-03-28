66975b8685442fddb49c49f488286eff1e39d6b1
Gary Miller <gary.miller@gmx.com>
2020-03-28 03:17:21 -0400

    Update gen_problems.rb
    Adds in test spec for the char_indices mehtods .

--- gary_miller_recap_exercise_3/spec/gen_problems_spec.rb
+++ gary_miller_recap_exercise_3/spec/gen_problems_spec.rb
@@ -29,3 +29,13 @@ describe "no_consecutive_repeats?" do
         expect(no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])).to eq(false)
     end
 end
+
+describe "char_indices" do
+    it "should return a hash containing keys equal to each character" do
+        expect(char_indices('mississippi')).to eq({"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]})
+    end
+
+    it "should contain values in an array of indices for the key in original string" do
+        expect(char_indices('classroom')).to eq({"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]})
+    end
+end
