6c52bb2144927cb37440e100e8d3ef778b419b09
Gary Miller <gary.miller@gmx.com>
2020-03-28 03:14:22 -0400

    Update gen_problems.rb
    Adds in the test specs for the no_dupes? solution.

--- gary_miller_recap_exercise_3/spec/gen_problems_spec.rb
+++ gary_miller_recap_exercise_3/spec/gen_problems_spec.rb
@@ -1,1 +1,15 @@
 require "gen_problems"
+
+describe "no_dupes?" do
+    it "should accept an array as an arg" do
+        expect(no_dupes?(['x', 'x', 'y', 'z', 'z'])).to eq(['y'])
+    end
+
+    it "should return an new array containing the elements that were not repeated" do
+        expect(no_dupes?([1, 1, 2, 1, 3, 2, 4])).to eq([3, 4])
+    end
+
+    it "should return an empty array if all elements were repeated" do
+        expect(no_dupes?([true, true, true])).to eq([])
+    end
+end
