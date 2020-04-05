205b71122cb520d59af536a07571f2c3b0699787
Gary Miller <gary.miller@gmx.com>
2020-04-05 11:19:33 -0400

    Update part_1.rb
    Adds in my solution to the nth_prime method.

--- rspec_exercise_3/lib/part_1.rb
+++ rspec_exercise_3/lib/part_1.rb
@@ -11,3 +11,12 @@ def is_prime?(num)
     else return (2...num).none? { |int| num % int == 0 }
     end
 end
+
+def nth_prime(num)
+    top = num * 10
+    primes = (2...top).select { |e| (2...e).none? { |i| e % i == 0 } }
+        if num == 1
+            return 2
+        else primes[num - 1]
+    end
+end
