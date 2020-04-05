98c3ac7fb7efa43719051fd023484923ea6977b9
Gary Miller <gary.miller@gmx.com>
2020-04-05 11:18:39 -0400

    Update part_1.rb
    Adds in my is_prime? method solution.

--- rspec_exercise_3/lib/part_1.rb
+++ rspec_exercise_3/lib/part_1.rb
@@ -2,3 +2,12 @@
     #   APP ACADEMY
     #   RSPEC EXERCISE 3
     #   PART 1
+
+def is_prime?(num)
+    if num < 2
+        return false
+    elsif num == 2
+        return true
+    else return (2...num).none? { |int| num % int == 0 }
+    end
+end
