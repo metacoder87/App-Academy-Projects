c3eb1cf471af0d855cbd7f7858c1d398888fb921
Gary Miller <gary.miller@gmx.com>
2020-04-08 23:14:56 -0400

    Update exercise.rb
    Adds in my solution to the zip method which can zip up to 3 arrays together.

--- rspec_exercise_5/lib/exercise.rb
+++ rspec_exercise_5/lib/exercise.rb
@@ -2,3 +2,14 @@
 
                 #   APP ACADEMY
                 #   RSPEC EXERCISE 5
+
+def zip(*arr)
+    arrays = []
+    arr.each { |array| arrays << array }
+    if arrays.length == 1
+        return arrays[0]
+    elsif arrays[2]
+        arrays[0].zip(arrays[1], arrays[2])
+    else arrays[0].zip(arrays[1])
+    end
+end
