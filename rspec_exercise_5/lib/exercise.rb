7487fddb4f8e5775061e360bac37244d9097afc4
Gary Miller <gary.miller@gmx.com>
2020-04-08 23:16:46 -0400

    Update exercise.rb
    Adds in my zany_zip method solution.

--- rspec_exercise_5/lib/exercise.rb
+++ rspec_exercise_5/lib/exercise.rb
@@ -17,3 +17,23 @@ end
 def prizz_proc(arr, proc1, proc2)
     arr.select { |ele| (proc1.call(ele) && !proc2.call(ele)) || (!proc1.call(ele) && proc2.call(ele)) }
 end
+
+def zany_zip(*arr)
+    arrays = []
+    arr.each { |array| arrays << array }
+    if arrays.length == 1
+        return arrays[0]
+    elsif arrays.length == 2 
+        arrays[0].zip(arrays[1])
+    elsif arrays.length == 3
+        while arrays[0].length < arrays[2].length
+            arrays[0] << nil
+        end
+        arrays[0].zip(arrays[1], arrays[2])
+    elsif arrays.length == 4
+    while arrays[0].length < arrays[3].length
+        arrays[0] << nil
+    end
+    arrays[0].zip(arrays[1], arrays[2], arrays[3])
+    end
+end
