43dbdda9a658579c005e5b9110697a70249bd7a4
Gary Miller <gary.miller@gmx.com>
2020-04-11 21:25:20 -0400

    Update flight.rb
    Adds in the passengers method solution.

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -3,10 +3,14 @@ require_relative 'passenger'
 
 class Flight
   
-      def initialize(flight_number, capacity)
+    def initialize(flight_number, capacity)
         @flight_number = flight_number
         @capacity = capacity
         @passengers = []
+    end
+  
+    def passengers
+        @passengers
     end
 
 end
