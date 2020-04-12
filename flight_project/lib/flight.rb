809ea450a3021f97ae21b9cdc71aeee098cf8ee1
Gary Miller <gary.miller@gmx.com>
2020-04-11 21:24:31 -0400

    Update flight.rb
    Adds in the initialize method solution for the flight class

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -2,5 +2,11 @@
 require_relative 'passenger'
 
 class Flight
+  
+      def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = []
+    end
 
 end
